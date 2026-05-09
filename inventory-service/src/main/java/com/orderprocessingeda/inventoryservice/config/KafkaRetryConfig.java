1bb451a4dc24e2b578269d4eb1aabe59dec8a8c4
Atulsinghyadav <atulsinghyadav36@gmail.com>
2026-05-09 19:59:42 +0530

    feat(Retry and DLQ): Added Retry mechanism with exponential backoff and implemented DLT handler)

---
+++ inventory-service/src/main/java/com/orderprocessingeda/inventoryservice/config/KafkaRetryConfig.java
@@ -0,0 +1,8 @@
+package com.orderprocessingeda.inventoryservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaRetryConfig {
+
+}
--- inventory-service/src/main/java/com/orderprocessingeda/inventoryservice/kafka/OrderConsumer.java
+++ inventory-service/src/main/java/com/orderprocessingeda/inventoryservice/kafka/OrderConsumer.java
@@ -5,7 +5,10 @@ import com.atulyadav.event.OrderItemEvent;
 import com.orderprocessingeda.inventoryservice.service.InventoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.BackOff;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -24,10 +27,12 @@ public class OrderConsumer {
         log.info("OrderConsumer bean created");
     }
 
+    @RetryableTopic(attempts = "3", backOff = @BackOff( delay = 2000, multiplier = 2))
     @KafkaListener(topics = "order-topic", groupId = "inventory-group")
     public void consume(OrderCreatedEvent event) {
 
         log.info("Received order event: {}", event.getOrderId());
+
         Long orderId = event.getOrderId();
 
         if(processedOrders.contains(orderId)){
@@ -44,6 +49,9 @@ public class OrderConsumer {
         log.info("Processed orderId={}", orderId);
     }
 
-
-
+    @DltHandler
+    public void handleDlt(OrderCreatedEvent event){
+        log.error("Message moved to DLT for orderId={}",
+                event.getOrderId());
+    }
 }
